59195a17ce63e855e931c40eaae47065b33222f1
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2025-06-10 16:26:25 -0600

    added common httpclient for connection

--- api/serverApi.js
+++ api/serverApi.js
@@ -1,1 +1,13 @@
 const axios = require('axios');
+
+const httpClient = axios.create({
+  baseURL: 'http://localhost:8080/cafediff', // base URL
+  timeout: 5000, //timeout for requests
+  headers: {
+    'Content-Type': 'application/json',
+    //add auth header later
+    
+  },
+});
+
+module.exports = httpClient;
